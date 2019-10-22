2d035c529177a947e1f243b32f240e8e0618e0fe
knowak_osds <knowak@openskydata.com>
2019-10-22 15:10:01 +0200

    add part of MongoContext

---
+++ todoProducts/DataAccess/Context/MongoContext.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Extensions.Configuration;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Conventions;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace todoProducts.DataAccess.Context
+{
+    public class MongoContext
+    {
+        private IMongoDatabase Database { get; set; }
+        private readonly List<Func<Task>> _commands;
+
+        public MongoContext(IConfiguration configuration)
+        {
+            BsonDefaults.GuidRepresentation = GuidRepresentation.CSharpLegacy; //?
+            _commands = new List<Func<Task>>();
+            RegisterConventions();
+
+            MongoClientSettings settings = new MongoClientSettings
+            {
+                Server = new MongoServerAddress(
+                    host: configuration.GetSection("MongoSettings").GetSection("Host").Value,
+                    port: int.Parse(configuration.GetSection("MongoSettings").GetSection("Port").Value)),
+                UseSsl = false,
+                Credential = MongoCredential.CreateCredential(
+                    databaseName: configuration.GetSection("MongoSettings").GetSection("DatabaseName").Value,
+                    username: configuration.GetSection("MongoSettings").GetSection("Username").Value,
+                    password: configuration.GetSection("MongoSettings").GetSection("Passowrd").Value)
+            };
+
+            var mongoClient = new MongoClient(settings);
+            Database = mongoClient.GetDatabase(configuration.GetSection("MongoSettings").GetSection("DatabaseName").Value);
+        }
+
+        private void RegisterConventions()
+        {
+            var pack = new ConventionPack
+            {
+                new IgnoreExtraElementsConvention(true),
+                new IgnoreIfDefaultConvention(true)
+            };
+            ConventionRegistry.Register("Solution Conventions", pack, t => true);
+        }
+
+        public async Task<int> SaveChanges()
+        {
+            var commandTasks = _commands.Select(c => c()); //? c()
+            await Task.WhenAll(commandTasks);
+            return _commands.Count; //? jak to czytać?
+        }
+
+        public IMongoCollection<T> GetCollection(string name)
+        {
+            return Database.GetCollection<T>(name);
+        }
+    }
+}
